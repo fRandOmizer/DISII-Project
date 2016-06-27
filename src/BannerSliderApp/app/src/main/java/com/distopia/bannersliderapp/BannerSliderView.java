a1e636cb6176edf6ced21ed1e34bbb804620da74
lukasw <foxnbk@gmail.com>
2016-06-27 23:55:49 +0200

    Finally, a working version! View pager now scales accordingly.
    The page slider can be set to have an offset from the bottom to allow for an alignment with the image.

--- src/BannerSliderApp/app/src/main/java/com/distopia/bannersliderapp/BannerSliderView.java
+++ src/BannerSliderApp/app/src/main/java/com/distopia/bannersliderapp/BannerSliderView.java
@@ -18,6 +18,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 
 /**
@@ -41,56 +42,28 @@ public class BannerSliderView extends LinearLayout {
      */
     private PagerAdapter mPagerAdapter;
 
-    private String mExampleString = "Bla"; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    private float mBottomOffset = 0.0f;
 
     public BannerSliderView(Context context) {
         super(context);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.banner_slider_view, this);
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (BannerSliderViewPager) findViewById(R.id.pager);
-        try {
-            FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
-            mPagerAdapter = new BannerSliderPagerAdapter(fragmentManager);
-        } catch (ClassCastException e) {
-            Log.e("BannerSliderView", "Can't get fragment manager");
-        }
-        mPager.setAdapter(mPagerAdapter);
-        mPager.setClipToPadding(false);
-        mPager.setPageMargin(-300);
-        mPager.setOffscreenPageLimit(2);
-        init(null, 0);
+        init(context, null, 0);
     }
 
     public BannerSliderView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.banner_slider_view, this);
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (BannerSliderViewPager) findViewById(R.id.pager);
-        try {
-            FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
-            mPagerAdapter = new BannerSliderPagerAdapter(fragmentManager);
-        } catch (ClassCastException e) {
-            Log.e("BannerSliderView", "Can't get fragment manager");
-        }
-        mPager.setAdapter(mPagerAdapter);
-        mPager.setClipToPadding(false);
-        mPager.setPageMargin(-300);
-        mPager.setOffscreenPageLimit(2);
-        init(attrs, 0);
+        init(context, attrs, 0);
     }
 
     public BannerSliderView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+
         LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.banner_slider_view, this);
+
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (BannerSliderViewPager) findViewById(R.id.pager);
         try {
@@ -99,128 +72,46 @@ public class BannerSliderView extends LinearLayout {
         } catch (ClassCastException e) {
             Log.e("BannerSliderView", "Can't get fragment manager");
         }
+
         mPager.setAdapter(mPagerAdapter);
         mPager.setClipToPadding(false);
-        mPager.setPageMargin(-300);
         mPager.setOffscreenPageLimit(2);
-        init(attrs, defStyle);
+        mPager.setCurrentItem(1);
+
+        if(attrs != null) {
+            TypedArray a = context.getTheme().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.BannerSliderView,
+                    0, 0);
+            try {
+                mBottomOffset = a.getFloat(R.styleable.BannerSliderView_bottomOffset, 0.0f);
+            } finally {
+                a.recycle();
+            }
+
+        }
+
+        findViewById(R.id.marginleft).getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mPager.setPadding(findViewById(R.id.marginleft).getWidth(), 0, findViewById(R.id.marginleft).getWidth(), (int)(mBottomOffset * findViewById(R.id.image).getHeight()));
+            }
+        });
+
+        findViewById(R.id.image).getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mPager.setPadding(findViewById(R.id.marginleft).getWidth(), 0, findViewById(R.id.marginleft).getWidth(), (int)(mBottomOffset * findViewById(R.id.image).getHeight()));
+            }
+        });
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.BannerSliderView, defStyle, 0);
-
-        mExampleString = a.getString(
-                R.styleable.BannerSliderView_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.BannerSliderView_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.BannerSliderView_exampleDimension,
-                mExampleDimension);
-
-        a.recycle();
-
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
+    public void setBottomOffset(float bottomOffset) {
+        mBottomOffset = bottomOffset;
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        /**super.onDraw(canvas);
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);**/
-    }
-
-    /**
-     * Gets the example string attribute value.
-     *
-     * @return The example string attribute value.
-     */
-    public String getExampleString() {
-        return mExampleString;
-    }
-
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
+    public float getBottomOffset() {
+        return mBottomOffset;
     }
 
     /**
@@ -240,11 +131,6 @@ public class BannerSliderView extends LinearLayout {
         @Override
         public int getCount() {
             return NUM_PAGES;
-        }
-
-        @Override
-        public float getPageWidth(int position) {
-            return 0.8f;
         }
     }
 }
