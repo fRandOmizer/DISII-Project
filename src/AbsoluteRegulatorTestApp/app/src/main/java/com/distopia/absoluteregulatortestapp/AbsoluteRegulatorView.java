888c6a715dda68ddf15d19274e2a9b09537bef51
Christoph Richter <christoph.richter@rwth-aachen.de>
2016-06-29 05:30:19 +0200

    AbsoluteRegulatorView: Introduced default custom view

---
+++ src/AbsoluteRegulatorTestApp/app/src/main/java/com/distopia/absoluteregulatortestapp/AbsoluteRegulatorView.java
@@ -0,0 +1,126 @@
+package com.distopia.absoluteregulatortestapp;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class AbsoluteRegulatorView extends View {
+    private Drawable mExampleDrawable;
+
+    private TextPaint mTextPaint;
+    private float mTextWidth;
+    private float mTextHeight;
+
+    public AbsoluteRegulatorView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public AbsoluteRegulatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public AbsoluteRegulatorView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.AbsoluteRegulatorView, defStyle, 0);
+
+        /*mExampleString = a.getString(
+                R.styleable.AbsoluteRegulatorView_exampleString);
+        mExampleColor = a.getColor(
+                R.styleable.AbsoluteRegulatorView_exampleColor,
+                mExampleColor);
+        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
+        // values that should fall on pixel boundaries.
+        mExampleDimension = a.getDimension(
+                R.styleable.AbsoluteRegulatorView_exampleDimension,
+                mExampleDimension);*/
+
+        if (a.hasValue(R.styleable.AbsoluteRegulatorView_exampleDrawable)) {
+            mExampleDrawable = a.getDrawable(
+                    R.styleable.AbsoluteRegulatorView_exampleDrawable);
+            mExampleDrawable.setCallback(this);
+        }
+
+        a.recycle();
+
+        // Set up a default TextPaint object
+        mTextPaint = new TextPaint();
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextAlign(Paint.Align.LEFT);
+
+        // Update TextPaint and text measurements from attributes
+        invalidateTextPaintAndMeasurements();
+    }
+
+    private void invalidateTextPaintAndMeasurements() {
+        mTextPaint.setTextSize(12);
+        mTextPaint.setColor(Color.BLACK);
+        mTextWidth = mTextPaint.measureText("Absolute Regulator");
+
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        mTextHeight = fontMetrics.bottom;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        // Draw the text.
+        canvas.drawText("Absolute Regulator",
+                paddingLeft + (contentWidth - mTextWidth) / 2,
+                paddingTop + (contentHeight + mTextHeight) / 2,
+                mTextPaint);
+
+        // Draw the example drawable on top of the text.
+        if (mExampleDrawable != null) {
+            mExampleDrawable.setBounds(paddingLeft, paddingTop,
+                    paddingLeft + contentWidth, paddingTop + contentHeight);
+            mExampleDrawable.draw(canvas);
+        }
+    }
+
+    /**
+     * Gets the example drawable attribute value.
+     *
+     * @return The example drawable attribute value.
+     */
+    public Drawable getExampleDrawable() {
+        return mExampleDrawable;
+    }
+
+    /**
+     * Sets the view's example drawable attribute value. In the example view, this drawable is
+     * drawn above the text.
+     *
+     * @param exampleDrawable The example drawable attribute value to use.
+     */
+    public void setExampleDrawable(Drawable exampleDrawable) {
+        mExampleDrawable = exampleDrawable;
+    }
+}
--- src/AbsoluteRegulatorTestApp/app/src/main/java/com/distopia/absoluteregulatortestapp/MainActivity.java
+++ src/AbsoluteRegulatorTestApp/app/src/main/java/com/distopia/absoluteregulatortestapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.distopia.absoluteregulatortestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.distopia.absoluteregulatortestapp.AbsoluteRegulatorView;
 
 public class MainActivity extends AppCompatActivity {
 
